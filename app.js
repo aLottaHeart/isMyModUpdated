0ac4f3766b9ac23a52816b54ead1169cbf268fe1
Felix Jonas Wiegleb <js-wiegleb@t-online.de>
2023-06-11 17:02:05 +0200

    Update ui

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
+const { reverse } = require('dns');
 
 const app = express();
 
@@ -17,7 +18,7 @@ if (!fs.existsSync(modsFile)) {
 }
 
 app.get('/', (req, res) => {
-    const mods = JSON.parse(fs.readFileSync(modsFile));
+    var mods = reverseObject(JSON.parse(fs.readFileSync(modsFile)));
     res.render('index', { mods });
 });
 
@@ -25,6 +26,7 @@ app.post('/update', async (req, res) => {
     const modIdentifier = req.body.modIdentifier;
     const response = await axios.get(`https://api.modrinth.com/v2/project/${modIdentifier}`);
     data = response.data;
+
     delete data.body;
     const mods = JSON.parse(fs.readFileSync(modsFile));
     mods[modIdentifier] = data;
@@ -37,3 +39,15 @@ app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
 
+function reverseObject(obj) {
+    const reversedObj = {};
+    const keys = Object.keys(obj);
+
+    // Iterate over the keys in reverse order
+    for (let i = keys.length - 1; i >= 0; i--) {
+        const key = keys[i];
+        reversedObj[key] = obj[key];
+    }
+
+    return reversedObj;
+}
